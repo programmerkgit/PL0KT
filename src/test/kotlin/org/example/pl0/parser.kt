b6ed73f28c909fd9e79e4beb9db5b0e9bd66968b
keishi kubo <admin@keishis-iMac.local>
2020-05-28 02:49:11 +0900

    change test

--- src/test/kotlin/org/example/pl0/parser.kt
+++ src/test/kotlin/org/example/pl0/parser.kt
@@ -27,77 +27,10 @@ import org.junit.Assert.assertEquals
  * */
 
 class TestParser {
-    @Test
-    fun testProgram1() {
-        val l = Lexer(
-            """
-var x, y;
-begin
-    x = 3;
-    y = 5;
-    write x + y; 
-end.
-
-        """.trimIndent()
-        )
-        val p = Parser(l)
-        val codes  = p.parse()
-        codes.forEachIndexed { i, v ->
-            println("i: $i $v")
-        }
-    }
-    @Test
-    fun testProgram2() {
-        val l = Lexer(
-            """
-function a(x, y) {
-    begin
-        return x * y;
-    end
-}
-begin
-    write a(2, 3); 
-    writeln; 
-end
-
-        """.trimIndent()
-        )
-        val p = Parser(l)
-        val codes = p.parse()
-        codes.forEachIndexed { i, v ->
-            println("i: $i $v")
-        }
-        println("start")
-        Executor(codes).execute()
-        println("done")
-    }
-
-    @Test
-    fun testFactor() {
-        val l = Lexer(
-            """
-function a(x, y) {
-    begin
-        write x;
-        write y;
-    end
-}
-begin
-    write a(2,3)
-end
-                
-        """.trimIndent()
-        )
-        val p = Parser(l)
-        val codes = p.parse()
-        codes.forEachIndexed { i, v ->
-            println("i: $i $v")
-        }
-    }
 
     @Test
     fun testConstDecl() {
-        val l = Lexer("const a = 3;")
+        val l = Lexer("const a = 3")
         val p = Parser(l)
         p.parse()
         assertEquals(true, true)
@@ -105,7 +38,7 @@ end
 
     @Test
     fun testVarDecl() {
-        val l = Lexer("var id, id2, id3;")
+        val l = Lexer("var id, id2, id3")
         val p = Parser(l)
         p.parse()
         assertEquals(true, true)
@@ -117,27 +50,5 @@ end
         val p = Parser(l)
         p.parse()
         assertEquals(true, true)
-    }
-
-    @Test
-    fun testProgram() {
-        val input = """
-var a, b;            
-fn a()
-   const a = 3;
-begin
- while b > 0 do
- begin
- end;
- if(1 = true) then 
-    begin r := r-w
-    end
- writeln(a);
-end;
-"""
-        val l = Lexer(input)
-        val p = Parser(l)
-        p.parse()
-        // assertEquals(true, true)
     }
 }
